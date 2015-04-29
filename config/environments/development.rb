31c437782466ccae8783c4bcb0dcbdefe0491820
Milos Arsenijevic <arsenijevicmilos@yahoo.com>
2015-04-29 03:12:54 +0200

    Add users and finish styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,7 @@ Rails.application.configure do
 
   #ImageMagic for gem Paperclip
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  #Needed for devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
